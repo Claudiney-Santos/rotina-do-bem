43f50ad6190af97ddb0b6b2a92fd61a38944d7e7
Claudiney Gustavo Rodrigues dos Santos <87202002+Claudiney-Santos@users.noreply.github.com>
2026-06-30 06:08:35 -0300

    feat: add about page

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -21,3 +21,7 @@ func _on_report_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_about_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/about.tscn")
